fc9440f2e811038a83fe1f43db2d4f5c7bc7fb3b
Alex Babits <alexbabits@gmail.com>
2023-06-28 09:00:24 -0400

    custom sensor and colliders and scale of monsters

--- js/Monsters.js
+++ js/Monsters.js
@@ -1,17 +1,24 @@
-// Monster.js
 export class Monster {
-
     static preload(scene) {
         scene.load.atlas('enemies', 'assets/images/enemies.png', 'assets/images/enemies_atlas.json');
         scene.load.animation('enemies_anims', 'assets/images/enemies_anims.json');
     }
     
-    constructor(scene, x, y, key, frame) {
+    constructor(scene, x, y, key, frame, colliderWidth, colliderHeight, chamfer, sensorRadius, scale) {
         this.scene = scene;
-        this.sprite = this.scene.matter.add.sprite(x, y, key, frame);
+        this.sprite = this.scene.matter.add.sprite(x, y, key, frame).setDepth(1).setScale(scale);
         // Add common properties here, such as health, speed, etc.
         // this.health = 100;
         // this.speed = 10;
+        
+        const {Body,Bodies} = Phaser.Physics.Matter.Matter;
+
+        this.collider = Bodies.rectangle(x, y, colliderWidth, colliderHeight, {chamfer: chamfer, isSensor: false, label:'monsterCollider'});
+        this.sensor = Bodies.circle(x, y, sensorRadius, {isSensor: true, label:'monsterSensor'});
+        const compoundBody = Body.create({parts:[this.collider, this.sensor], frictionAir: .4});
+        
+        this.sprite.setExistingBody(compoundBody);
+        this.sprite.setFixedRotation();
     }
 
     update() {
@@ -22,8 +29,8 @@ export class Monster {
 
 export class Bear extends Monster {
     constructor(scene, x, y, key = 'enemies', frame) {
-        super(scene, x, y, key, frame);
-        this.sprite.play('bear_idle'); // assuming this is the name of the animation
+        super(scene, x, y, key, frame, 50, 35, {radius: [15, 15, 15, 15]}, 75, 0.75); 
+        this.sprite.play('bear_idle'); 
         // Add bear-specific properties here, such as attack strength, etc.
         // this.attackStrength = 20;
     }
@@ -36,8 +43,8 @@ export class Bear extends Monster {
 
 export class Ent extends Monster {
     constructor(scene, x, y, key = 'enemies', frame) {
-        super(scene, x, y, key, frame);
-        this.sprite.play('ent_idle'); // assuming this is the name of the animation
+        super(scene, x, y, key, frame, 20, 50, {radius: [5, 5, 5, 5]}, 60, 0.85);  
+        this.sprite.play('ent_idle'); 
         // Add ent-specific properties here, such as attack strength, etc.
         // this.attackStrength = 15;
     }
@@ -47,4 +54,3 @@ export class Ent extends Monster {
         // Ent-specific update logic goes here
     }
 }
-
