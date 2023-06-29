079818bad7e8c9c33c4e2aa64a6ddfb915642db5
Alex Babits <alexbabits@gmail.com>
2023-06-29 08:44:34 -0400

    to-do cleaned up, monster comments removed

--- js/Monsters.js
+++ js/Monsters.js
@@ -7,9 +7,6 @@ export class Monster {
     constructor(scene, x, y, key, frame, colliderWidth, colliderHeight, chamfer, sensorRadius, scale) {
         this.scene = scene;
         this.sprite = this.scene.matter.add.sprite(x, y, key, frame).setDepth(1).setScale(scale);
-        // Add common properties here, such as health, speed, etc.
-        // this.health = 100;
-        // this.speed = 10;
         
         const {Body,Bodies} = Phaser.Physics.Matter.Matter;
 
@@ -22,7 +19,6 @@ export class Monster {
     }
 
     update() {
-        // Common update logic goes here
     }
 }
 
@@ -31,13 +27,10 @@ export class Bear extends Monster {
     constructor(scene, x, y, key = 'enemies', frame) {
         super(scene, x, y, key, frame, 47, 35, {radius: [18, 21, 20, 12]}, 75, 0.75); 
         this.sprite.play('bear_idle'); 
-        // Add bear-specific properties here, such as attack strength, etc.
-        // this.attackStrength = 20;
     }
 
     update() {
         super.update();
-        // Bear-specific update logic goes here
     }
 }
 
@@ -45,12 +38,9 @@ export class Ent extends Monster {
     constructor(scene, x, y, key = 'enemies', frame) {
         super(scene, x, y, key, frame, 20, 45, {radius: [7, 7, 7, 7]}, 60, 0.85);  
         this.sprite.play('ent_idle'); 
-        // Add ent-specific properties here, such as attack strength, etc.
-        // this.attackStrength = 15;
     }
 
     update() {
         super.update();
-        // Ent-specific update logic goes here
     }
 }
