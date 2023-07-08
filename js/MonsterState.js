6368a0150c0df5a1585566782de476bff9e19890
Alex Babits <alexbabits@gmail.com>
2023-07-08 11:13:36 -0400

    monster 1st attack speed elegantly fixed

--- js/MonsterState.js
+++ js/MonsterState.js
@@ -49,18 +49,8 @@ export class MonsterAttackingState extends MonsterState {
         console.log(`${this.monster.name} entered attacking state (idle anim atm)`);
         this.monster.sprite.setVelocity(0, 0);
         this.monster.sprite.play(this.monster.idleAnim);
-        if (!this.monster.attackCooldown || this.monster.scene.time.now > this.monster.attackCooldown) {
-            this.monster.player.gameState.setPlayerHP(this.monster.player.gameState.getPlayerHP() - this.monster.Damage);
-            console.log(`${this.monster.name} attacked the player for ${this.monster.Damage} Damage. Player health: ${this.monster.player.gameState.getPlayerHP()}`);
-            this.attackTimer = this.monster.scene.time.now;
-            this.monster.attackCooldown = this.monster.scene.time.now + (1000 * this.monster.attackSpeed);
-
-            if(this.monster.player.gameState.getPlayerHP() > 0) {
-                this.monster.player.transitionToNewState(this.monster.player.gotHitState);
-            } else {
-                this.monster.player.transitionToNewState(this.monster.player.deathState);
-            }
-        }
+        this.attackTimer = this.monster.scene.time.now;
+        this.firstAttack = true;
     }
 
     update(player) {
@@ -80,11 +70,14 @@ export class MonsterAttackingState extends MonsterState {
             return;
         }
 
-        if (this.monster.scene.time.now - this.attackTimer > (1000*this.monster.attackSpeed) && this.monster.scene.time.now > this.monster.attackCooldown) {
+        let attackSpeed = this.firstAttack ? this.monster.attackSpeed / 4 : this.monster.attackSpeed;
+
+        if (this.monster.scene.time.now - this.attackTimer > (1000 * attackSpeed)) {
             player.gameState.setPlayerHP(player.gameState.getPlayerHP() - this.monster.Damage);
             console.log(`${this.monster.name} attacked the player for ${this.monster.Damage} Damage. Player health: ${player.gameState.getPlayerHP()}`);
             this.attackTimer = this.monster.scene.time.now;
             this.monster.attackCooldown = this.monster.scene.time.now + (1000 * this.monster.attackSpeed);
+            this.firstAttack = false;
 
             if(player.gameState.getPlayerHP() > 0) {
                 player.transitionToNewState(player.gotHitState);
@@ -95,6 +88,7 @@ export class MonsterAttackingState extends MonsterState {
     }
 
     exit() {
+        this.firstAttack = false;
         this.attackTimer = null;
     }
 }
