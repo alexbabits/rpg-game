8ee9ffa6c52c889844b849ee276ba1154849c0de
Alex Babits <alexbabits@gmail.com>
2023-07-26 23:07:58 -0400

    cleanup/prep for load/new/save etc.

--- js/Menu.js
+++ js/Menu.js
@@ -131,13 +131,13 @@ export class Controls extends Phaser.Scene {
         this.returnScene = data.returnScene;
         this.controlsBackground = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, 'controlsbackground');
 
-        this.add.text(320, 100, 'WASD/Arrow Keys - Move', { fontSize: '32px', fontFamily: 'Arial', fill: '#000', resolution: 4 }).setOrigin(0.5, 0.5);
-        this.add.text(320, 175, 'I - Inventory', { fontSize: '32px', fontFamily: 'Arial', fill: '#000', resolution: 4 }).setOrigin(0.5, 0.5);
-        this.add.text(320, 250, 'C - Character', { fontSize: '32px', fontFamily: 'Arial', fill: '#000', resolution: 4 }).setOrigin(0.5, 0.5);
-        this.add.text(320, 325, 'Space - Attack', { fontSize: '32px', fontFamily: 'Arial', fill: '#000', resolution: 4 }).setOrigin(0.5, 0.5);
-        this.add.text(320, 400, 'Ctrl+Space - Special Attack', { fontSize: '32px', fontFamily: 'Arial', fill: '#000', resolution: 4 }).setOrigin(0.5, 0.5);
-        this.add.text(320, 475, 'Shift - Run', { fontSize: '32px', fontFamily: 'Arial', fill: '#000', resolution: 4 }).setOrigin(0.5, 0.5);
-        this.add.text(320, 550, 'Esc - Exit/Back', { fontSize: '32px', fontFamily: 'Arial', fill: '#000', resolution: 4 }).setOrigin(0.5, 0.5);
+        this.add.text(320, 100, 'WASD/Arrow Keys - Move', { fontSize: '32px', fontFamily: 'Arial', fill: '#cbdbfc', resolution: 4 }).setOrigin(0.5, 0.5);
+        this.add.text(320, 175, 'I - Inventory', { fontSize: '32px', fontFamily: 'Arial', fill: '#cbdbfc', resolution: 4 }).setOrigin(0.5, 0.5);
+        this.add.text(320, 250, 'C - Character', { fontSize: '32px', fontFamily: 'Arial', fill: '#cbdbfc', resolution: 4 }).setOrigin(0.5, 0.5);
+        this.add.text(320, 325, 'Space - Attack', { fontSize: '32px', fontFamily: 'Arial', fill: '#cbdbfc', resolution: 4 }).setOrigin(0.5, 0.5);
+        this.add.text(320, 400, 'Ctrl+Space - Special Attack', { fontSize: '32px', fontFamily: 'Arial', fill: '#cbdbfc', resolution: 4 }).setOrigin(0.5, 0.5);
+        this.add.text(320, 475, 'Shift - Run', { fontSize: '32px', fontFamily: 'Arial', fill: '#cbdbfc', resolution: 4 }).setOrigin(0.5, 0.5);
+        this.add.text(320, 550, 'Esc - Exit/Back', { fontSize: '32px', fontFamily: 'Arial', fill: '#cbdbfc', resolution: 4 }).setOrigin(0.5, 0.5);
 
 
         this.input.keyboard.on('keydown-ESC', () => {
