ba72420197f1c8f6509649f387000cb2a413e757
Alex Babits <alexbabits@gmail.com>
2023-08-03 10:48:56 -0400

    mostly good,need refactor LoadScene

--- js/LoadScene.js
+++ js/LoadScene.js
@@ -27,11 +27,16 @@ export default class LoadScene extends Phaser.Scene {
             let buttonClickedMethod;
             const y = yPos[index];
             const buttonRectangle = this.add.rectangle(this.game.config.width / 2, y, 600, 160, 0xcbdbfc).setInteractive();
-            buttonRectangle.on('pointerover', () => buttonRectangle.setFillStyle(0xa3bffa));
-            buttonRectangle.on('pointerout', () => buttonRectangle.setFillStyle(0xcbdbfc));
+            const loadButtonX = this.game.config.width / 2 + 200;
+            const loadButtonY = y - 40;
+            const loadButtonRectangle = this.add.rectangle(loadButtonX, loadButtonY, 100, 50, 0x00FF00).setInteractive();
+            const loadButtonText = this.add.text(loadButtonX, loadButtonY, 'Load', { fontSize: '24px', fontFamily: 'Arial', fill: '#452840', resolution: 4 }).setOrigin(0.5, 0.5);
+
+            loadButtonRectangle.on('pointerover', () => loadButtonRectangle.setFillStyle(0x023020));
+            loadButtonRectangle.on('pointerout', () => loadButtonRectangle.setFillStyle(0x00FF00));
     
             const buttonText = this.add.text(120, y, label, { fontSize: '48px', fontFamily: 'Arial', fill: '#452840', resolution: 4 }).setOrigin(0.5, 0.5);
-            const buttonContainer = this.add.container(0, 0, [buttonRectangle, buttonText]);
+            const buttonContainer = this.add.container(0, 0, [buttonRectangle, buttonText, loadButtonRectangle, loadButtonText]);
     
             if (label === `Slot 1`) {
                 buttonClickedMethod = this.loadGame1.bind(this);
@@ -52,16 +57,17 @@ export default class LoadScene extends Phaser.Scene {
                 else if (label === 'Slot 3') { buttonClickedMethod = this.loadGame3.bind(this); }
             }
     
-            buttonRectangle.on('pointerdown', buttonClickedMethod);
+            loadButtonRectangle.on('pointerdown', buttonClickedMethod);
         });
         this.setupDeleteButton(stats);
     }
 
     setupDeleteButton(stats) {
-        const deleteButtonRectangle = this.add.rectangle(500, 100, 100, 50, 0xff0000).setInteractive();
-        const deleteButtonText = this.add.text(500, 100, 'Delete', { fontSize: '24px', fontFamily: 'Arial', fill: '#fff', resolution: 4 }).setOrigin(0.5, 0.5);
+        const deleteButtonRectangle = this.add.rectangle(520, 130, 100, 50, 0xff0000).setInteractive();
+        const deleteButtonText = this.add.text(520, 130, 'Delete', { fontSize: '24px', fontFamily: 'Arial', fill: '#fff', resolution: 4 }).setOrigin(0.5, 0.5);
         const deleteButtonContainer = this.add.container(0, 0, [deleteButtonRectangle, deleteButtonText]);
-    
+        deleteButtonRectangle.on('pointerover', () => deleteButtonRectangle.setFillStyle(0x8B0000));
+        deleteButtonRectangle.on('pointerout', () => deleteButtonRectangle.setFillStyle(0xff0000));
         if (stats !== null) {
             deleteButtonRectangle.on('pointerdown', async () => {
                 await this.gameState.deleteSave();
@@ -72,6 +78,15 @@ export default class LoadScene extends Phaser.Scene {
             deleteButtonText.setAlpha(0.5);
         }
     }
+    
+    async deleteGame1(){
+        //handles the functionality of deleting the game from slot 1.
+    }
+
+    async deleteGame2(){console.log('Delete Slot 2 button pressed. Dummy button for now.')}
+    async deleteGame3(){console.log('Delete Slot 3 button pressed. Dummy button for now.')}
+
+    // Methods for loading the game
 
     async loadGame1(){
         console.log('Load button pressed');
@@ -98,7 +113,7 @@ export default class LoadScene extends Phaser.Scene {
         console.log('Game Loaded');
     }
 
-    loadGame2(){console.log('Load Slot 2 button pressed. Dummy button for now.')}
-    loadGame3(){console.log('Load Slot 3 button pressed. Dummy button for now.')}
+    async loadGame2(){console.log('Load Slot 2 button pressed. Dummy button for now.')}
+    async loadGame3(){console.log('Load Slot 3 button pressed. Dummy button for now.')}
 
 }
