84ff9d45a973a41a07c18ff84866affedbf7b6d2
Alex Babits <alexbabits@gmail.com>
2023-08-04 16:53:51 -0400

    Multiple messages scrolling/deleteing good

--- js/MessageBox.js
+++ js/MessageBox.js
@@ -1,6 +1,8 @@
 export default class MessageBox extends Phaser.Scene {
     constructor() {
         super("MessageBox");
+        this.messages = [];
+        this.maxMessages = 6;
     }
 
     init(data) {
@@ -11,11 +13,20 @@ export default class MessageBox extends Phaser.Scene {
         this.gameState = data.gameState;
         this.background = this.add.sprite(130, 480, 'brownbackground').setScale(3, 2);
         this.background.alpha = 1;
-        this.messageText = this.add.text(20,520, '', {font: "16px Arial", fill: "#000", resolution: 2});
         this.equipmentData.on('message', this.updateMessage, this);
     }
 
     updateMessage(message) {
-        this.messageText.setText(message);
+        for (let i = 0; i < this.messages.length; i++) {
+            this.messages[i].y -= 20;
+        }
+
+        const newMessage = this.add.text(20, 520, message, {font: "16px Arial", fill: "#000", resolution: 2});
+        this.messages.push(newMessage);
+
+        if (this.messages.length > this.maxMessages) {
+            this.messages[0].destroy();
+            this.messages.shift();
+        }
     }
 }
